13ca0b4a387184487100fd03284867c455b8af52
Andreas Blanke <blan@itu.dk>
2017-03-10 11:54:56 +0100

    Refactoring of the View package (a few being finished)

--- src/View/ToolComponent.java
+++ src/View/ToolComponent.java
@@ -5,17 +5,22 @@ import javax.swing.*;
 /**
  * Class details:
  *
+ *
  * @author Andreas Blanke, blan@itu.dk
+ * @author Niclas Hedam, nhed@itu.dk
+ * @author Oracle Team
  * @version 06-03-2017.
- * @project BFST
  */
 abstract class ToolComponent extends JPanel {
 
     abstract void setupLayout();
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    ImageIcon createImageIcon(String path,
-                              String description) {
+    /**
+     * Returns an ImageIcon, or null if the path was invalid.
+     * @see <a href="https://docs.oracle.com/javase/tutorial/uiswing/components/icon.html">
+     *     Dublicated code from this tutorial made by the Oracle Team</a>
+     */
+    ImageIcon createImageIcon(String path, String description) {
         java.net.URL imgURL = getClass().getResource(path);
         if (imgURL != null) {
             return new ImageIcon(imgURL, description);
