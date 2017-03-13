1893a1747737155c6f729d0debce7df7bc2c16eb
Nikolaj Bl <nibl@itu.dk>
2017-03-13 14:14:40 +0100

    Continued working with blackbox testing

--- tests/TestOSMHandler.java
+++ tests/TestOSMHandler.java
@@ -1,7 +1,7 @@
 import Controller.CanvasController;
 import Controller.InfobarController;
 import Controller.ToolbarController;
-import Enums.WayType;
+import Enums.OSMEnums.WayType;
 import Helpers.FileHandler;
 import View.Window;
 import junit.framework.TestCase;
@@ -12,7 +12,7 @@ import Model.*;
  */
 public class TestOSMHandler extends TestCase {
 
-    public void testOSMHandler(){
+    public void testRoads(){
         Window window = new Window();
         Model model = Model.getInstance();
         CanvasController canvasController = new CanvasController(window);
@@ -24,7 +24,7 @@ public class TestOSMHandler extends TestCase {
         assertEquals(1, RoadCount);
     }
 
-    public void testOSMHandlerWithReload(){
+    public void testReload(){
         Window window = new Window();
         Model model = Model.getInstance();
         CanvasController canvasController = new CanvasController(window);
@@ -34,5 +34,16 @@ public class TestOSMHandler extends TestCase {
         FileHandler.loadDefault("/testRoad.osm");
         int RoadCount = model.getWayElements().get(WayType.ROAD).size();
         assertEquals(1, RoadCount);
+    }
+
+    public void testUnknown(){
+        Window window = new Window();
+        Model model = Model.getInstance();
+        CanvasController canvasController = new CanvasController(window);
+        ToolbarController toolbarController = ToolbarController.getInstance(window);
+        InfobarController infobarController = new InfobarController(window);
+        FileHandler.loadDefault("/testUnknown.osm");
+        int UnknownCount = model.getWayElements().get(WayType.UNKNOWN).size();
+        assertEquals(1, UnknownCount);
     }
 }
