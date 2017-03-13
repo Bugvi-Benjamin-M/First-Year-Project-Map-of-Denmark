631c4e80885b2a07484361c89d3b0339d72b2338
Nikolaj Bl <nibl@itu.dk>
2017-03-13 15:10:10 +0100

    Added a fileExists method and a fileNotFoundException

---
+++ src/Exceptions/FileNotFoundException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+/**
+ * Created by Nik on 13/03/17.
+ */
+public class FileNotFoundException extends Exception{
+    public FileNotFoundException(String message){
+        super(message);
+    }
+}
