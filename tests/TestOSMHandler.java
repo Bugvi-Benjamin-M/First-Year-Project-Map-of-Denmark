73766078ea9c9e8ee1033f30eca1dc9d900a7de8
Nikolaj Bl <nibl@itu.dk>
2017-03-13 14:50:35 +0100

    Added a bounds test method

--- tests/TestOSMHandler.java
+++ tests/TestOSMHandler.java
@@ -3,6 +3,7 @@ import Controller.InfobarController;
 import Controller.ToolbarController;
 import Enums.OSMEnums.WayType;
 import Helpers.FileHandler;
+import OSM.OSMHandler;
 import View.Window;
 import junit.framework.TestCase;
 import Model.*;
@@ -36,7 +37,7 @@ public class TestOSMHandler extends TestCase {
         assertEquals(1, RoadCount);
     }
 
-    public void testUnknown(){
+    public void testUnknown() {
         Window window = new Window();
         Model model = Model.getInstance();
         CanvasController canvasController = new CanvasController(window);
@@ -45,5 +46,19 @@ public class TestOSMHandler extends TestCase {
         FileHandler.loadDefault("/testUnknown.osm");
         int UnknownCount = model.getWayElements().get(WayType.UNKNOWN).size();
         assertEquals(1, UnknownCount);
+    }
+
+    public void testBounds(){
+        Window window = new Window();
+        Model model = Model.getInstance();
+        CanvasController canvasController = new CanvasController(window);
+        ToolbarController toolbarController = ToolbarController.getInstance(window);
+        InfobarController infobarController = new InfobarController(window);
+        FileHandler.loadDefault("/testUnknown.osm");
+        assertTrue(Math.abs(-55.7272800 - model.getMinLatitude()) < 0.0001);
+        assertTrue(Math.abs(11.4692700 * OSMHandler.getInstance().getLongitudeFactor() - model.getMinLongitude()) < 0.0001);
+        assertTrue(Math.abs(-55.7286100 - model.getMaxLatitude()) < 0.0001);
+        assertTrue(Math.abs(11.4735500 * OSMHandler.getInstance().getLongitudeFactor() - model.getMaxLongitude()) < 0.0001);
+
     }
 }
