9de02d2394c244b001bdbdf5f0fa28e472ffaab5
Búgvi Magnussen <buma@itu.dk>
2017-03-16 22:47:24 +0100

    Added two tests to TestModel

--- tests/TestModel.java
+++ tests/TestModel.java
@@ -1,18 +1,38 @@
+import Helpers.FileHandler;
 import junit.framework.TestCase;
 import Model.*;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+
 
 public class TestModel extends TestCase {
 
+    private Model model;
+    private Model model2;
 
     public TestModel() {
-
+        model = Model.getInstance();
+        model2 = Model.getInstance();
+        try {
+            FileHandler.loadDefault("/testRoad.osm");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void testPrint() {
-        System.out.println("Niclas er dum");
+    public void testSingleton() {
+        assertTrue(model.equals(model2));
+    }
+
+    @Test
+    public void testClear() {
+
+        assertFalse(model.getWayElements().isEmpty());
+        model.clear();
+        assertTrue(model.getWayElements().isEmpty());
+
     }
 
 }
