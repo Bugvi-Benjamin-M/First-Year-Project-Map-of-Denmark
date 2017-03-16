9e6be007e5313c84fda9c8a9aea7185babe54ac1
Búgvi Magnussen <buma@itu.dk>
2017-03-16 22:55:55 +0100

    Added two tests to TestModel

--- tests/TestModel.java
+++ tests/TestModel.java
@@ -1,4 +1,6 @@
+import Controller.CanvasController;
 import Helpers.FileHandler;
+import View.Window;
 import junit.framework.TestCase;
 import Model.*;
 import org.junit.Test;
@@ -10,10 +12,15 @@ public class TestModel extends TestCase {
 
     private Model model;
     private Model model2;
+    private CanvasController canvasController;
+    private Window window;
 
     public TestModel() {
+        window = new Window();
         model = Model.getInstance();
         model2 = Model.getInstance();
+        canvasController = CanvasController.getInstance(window);
+
         try {
             FileHandler.loadDefault("/testRoad.osm");
         } catch (FileNotFoundException e) {
