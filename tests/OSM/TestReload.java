9c00983e1b236c01e7625c914824e4de87d7fe6a
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-03-22 11:47:19 +0100

    Test fix. All tests in separate classes.
    Fixed test reload in OSMHandler

--- tests/OSM/TestReload.java
+++ tests/OSM/TestReload.java
@@ -6,13 +6,11 @@ import Controller.WindowController;
 import Enums.OSMEnums.WayType;
 import Helpers.FileHandler;
 import Model.Model;
-import OSM.OSMHandler;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Nik on 13/03/17.
@@ -30,7 +28,13 @@ public class TestReload {
         }
         int RoadCount = model.getWayElements().get(WayType.ROAD).size();
         assertEquals(3, RoadCount);
-
+        try {
+            FileHandler.loadDefault("/testRoad.osm");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        RoadCount = model.getWayElements().get(WayType.ROAD).size();
+        assertEquals(1, RoadCount);
         mainWindowController.resetInstance();
         model.resetInstance();
         canvasController.resetInstance();
