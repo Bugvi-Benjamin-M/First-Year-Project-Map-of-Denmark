c355c8cb5d03812cbca03fb900f5176c2d8ca5a9
Niclas Hedam <niclas@frax.dk>
2017-03-27 16:52:58 +0200

    Made BST tests ready

---
+++ Tests/TestBST.java
@@ -0,0 +1,22 @@
+import Model.BST;
+import static junit.framework.TestCase.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBST{
+
+    private BST bst;
+
+    @Before
+    public void setUp(){
+        BST bst = new BST();
+        //bst.initialize();
+    }
+
+    @Test
+    public void test(){
+        assertEquals(true, true); //TODO
+    }
+
+}
