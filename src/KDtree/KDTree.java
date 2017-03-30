b16abef8fa4a898e7d3857c872e1a2ec592f0d53
Jakob Eik Thunbo Mollerup <jmol@itu.dk>
2017-03-30 15:02:46 +0200

    Bugvi din computer lugter værre end bræk

--- src/KDtree/KDTree.java
+++ src/KDtree/KDTree.java
@@ -4,4 +4,37 @@ package KDtree;
  * Created by Jakob on 30-03-2017.
  */
 public class KDTree {
+    private Node root;
+    private static final int AMOUNT_OF_NODES_DEFAULT = 18788597;
+    private static final int DEPTH_DEFAULT = 14;
+
+    public KDTree(){
+
+    }
+
+    public void putNode(Node node){
+        root = putNode(root, node);
+    }
+
+    private Node putNode(Node parent, Node nodeToPut){
+        if(parent.getDepth() % 2 == 0){
+            int compare = nodeToPut.compareToX(parent);
+            if(compare <= 0) parent.setLeft(putNode(parent.getLeft(), nodeToPut));
+            if(compare > 0) parent.setRight(putNode(parent.getRight(), nodeToPut));
+        }
+        else{
+            int compare = nodeToPut.compareToY(parent);
+            if(compare <= 0) parent.setLeft(putNode(parent.getLeft(), nodeToPut));
+            if(compare > 0) parent.setRight(putNode(parent.getRight(), nodeToPut));
+        }
+        return parent;
+    }
+
+    public void putPointer(Pointer pointer){
+        putPointer(root, pointer);
+    }
+
+    private void putPointer(Node currentNode, Pointer pointer){
+
+    }
 }
--- src/KDtree/Node.java
+++ src/KDtree/Node.java
@@ -1,7 +1,46 @@
 package KDtree;
 
+import java.util.*;
+
 /**
  * Created by Jakob on 30-03-2017.
  */
-public class Node {
+public class Node extends Point{
+    private List<Pointer> pointers;
+    private int depth;
+    private Node left;
+    private Node right;
+
+    public Node(float x, float y, int depth){
+        super(x, y);
+        this.depth = depth;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void addPointer(Pointer pointer){
+        pointers.add(pointer);
+    }
+
+    public void makeLeaf(){
+        pointers = new ArrayList<>();
+    }
 }
