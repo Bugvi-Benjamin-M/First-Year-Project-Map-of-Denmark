8869dcde80aaf8f0181c275431e8f1391e739b71
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-03-31 11:52:54 +0200

    Further refactoring of KDTree

--- src/KDtree/NodeGenerator.java
+++ src/KDtree/NodeGenerator.java
@@ -32,11 +32,98 @@ public class NodeGenerator {
         points = new Point2D.Float[amountOfNodes];
     }
 
+    public void initialise() {
+
+    }
+
+    private void initialise(Node parent, int low, int high) {
+
+    }
+
     public void addPoint(Point2D.Float point) {
         points[pointsIndex] = point;
         pointsIndex++;
     }
 
+    private Point2D.Float findMedianX(int low, int high) {
+        int median = (low + high) / 2;
+        while (high > low) {
+            int j = partitionX(low, high);
+            if (j == median) return points[median];
+            else if(j > median) high = j - 1;
+            else if(j < median) low = j + 1;
+        }
+        return points[median];
+    }
 
+    private Point2D.Float findMedianY(int low, int high) {
+        int median = (low + high) / 2;
+        while (high > low) {
+            int j = partitionY(low, high);
+            if (j == median) return points[median];
+            else if(j > median) high = j - 1;
+            else if(j < median) low = j + 1;
+        }
+        return points[median];
+    }
 
+    private int partitionX(int low, int high) {
+        int i = low;
+        int j = high + 1;
+        Point2D.Float v = points[low];
+        while (true) {
+            while (lessX(points[++i], v)) if(i == high) break;
+            while (lessX(v, points[--j])) if(j == low) break;
+            if(i >= j) break;
+            exchange(i,j);
+        }
+        exchange(low , j);
+        return j;
+    }
+
+    private int partitionY(int low, int high) {
+        int i = low;
+        int j = high + 1;
+        Point2D.Float v = points[low];
+        while (true) {
+            while (lessY(points[++i], v)) if(i == high) break;
+            while (lessY(v, points[--j])) if(j == low) break;
+            if(i >= j) break;
+            exchange(i,j);
+        }
+        exchange(low , j);
+        return j;
+    }
+
+    private boolean lessX(Point2D.Float a, Point2D.Float b) {
+        Double x1 = a.getX();
+        Double x2 = b.getX();
+        Double y1 = a.getY();
+        Double y2 = b.getY();
+        if(x1.compareTo(x2) == 0) {
+            if(y1.compareTo(y2) == 0) return false;
+            else return y1.compareTo(y2) < 0;
+        } else {
+            return x1.compareTo(x2) < 0;
+        }
+    }
+
+    private boolean lessY(Point2D.Float a, Point2D.Float b) {
+        Double x1 = a.getX();
+        Double x2 = b.getX();
+        Double y1 = a.getY();
+        Double y2 = b.getY();
+        if(y1.compareTo(y2) == 0) {
+            if(x1.compareTo(x2) == 0) return false;
+            else return x1.compareTo(x2) < 0;
+        } else {
+            return y1.compareTo(y2) < 0;
+        }
+    }
+
+    private void exchange(int i, int j) {
+        Point2D.Float temp = points[i];
+        points[i] = points[j];
+        points[j] = temp;
+    }
 }
