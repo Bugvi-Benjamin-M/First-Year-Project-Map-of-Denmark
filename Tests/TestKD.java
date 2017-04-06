dd497ea3825d28d0aaf06c67bc8e8a142114f398
Niclas Hedam <niclas@frax.dk>
2017-04-06 11:52:39 +0200

    a failing test

--- Tests/TestKD.java
+++ Tests/TestKD.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import Model.Elements.*;
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.HashSet;
+
 
 
 import static junit.framework.TestCase.assertEquals;
@@ -20,7 +22,27 @@ public class TestKD {
 
     @Before
     public void buildUp(){
+        NodeGenerator gen = new NodeGenerator(10, 2);
         tree = new KDTree();
+
+        for(int i = 0; i < 10; i++){
+            gen.addPoint(new Point2D.Float(i, i * 2));
+        }
+
+        gen.initialise();
+        gen.setupTree(tree);
+
+        for(int i = 0; i < 10; i++){
+            Path2D rectangle = new Path2D.Double();
+            Element el = new UnknownWay(rectangle);
+            tree.putPointer(new Pointer(i, i * 2, el));
+        }
+    }
+
+    @Test
+    public void testGetManyElements(){
+        HashSet<Element> set = tree.getManyElements(-1, -1, 3, 3);
+        assertEquals(2, set.size());
     }
 
     @Test
