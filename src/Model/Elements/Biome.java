7ef0edae54cf0f6d9125c42b5c2bac87087891d8
Nikolaj Bl <nibl@itu.dk>
2017-04-14 19:08:28 +0200

    small changes

---
+++ src/Model/Elements/Biome.java
@@ -0,0 +1,28 @@
+package Model.Elements;
+
+import Helpers.Shapes.PolygonApprox;
+
+import java.awt.geom.Path2D;
+
+/**
+ * Created by Nik on 12/04/17.
+ */
+public class Biome extends Element{
+    private String name;
+
+    public Biome(PolygonApprox polygon, String name){
+        super(polygon);
+        this.name = name;
+    }
+    public Biome(PolygonApprox polygon){
+        this(polygon, "");
+    }
+
+    public PolygonApprox getShape(){
+        return (PolygonApprox) super.getShape();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
