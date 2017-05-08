d50cff29a638d3efca2a4613102b23d2e3a52368
Niclas Hedam <niclas@frax.dk>
2017-05-08 14:59:31 +0200

    FontAwesome optimization

--- src/Helpers/FontAwesome.java
+++ src/Helpers/FontAwesome.java
@@ -5,13 +5,18 @@ import java.io.InputStream;
 
 public class FontAwesome {
 
+    public static Font fontAwesome;
     public static Font getFontAwesome()
     {
+        if(fontAwesome != null){
+            return fontAwesome;
+        }
+
         try {
             InputStream is = FontAwesome.class.getResourceAsStream("/fa.ttf");
             Font font = Font.createFont(Font.TRUETYPE_FONT, is);
-            font = font.deriveFont(Font.PLAIN, 38f);
-            return font;
+            fontAwesome = font.deriveFont(Font.PLAIN, 38f);
+            return fontAwesome;
         } catch (Exception e) {
             throw new RuntimeException("FontAwesome could not be loaded.");
         }
