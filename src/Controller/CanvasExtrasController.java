f55a35ed1746a6a28ed93d888c43c2305ccdfc5c
Andreas Blanke <andreas.blanke.dk@gmail.com>
2017-05-15 20:24:29 +0200

    javadoc for canvas extras controller

--- src/Controller/CanvasExtrasController.java
+++ src/Controller/CanvasExtrasController.java
@@ -6,7 +6,11 @@ import View.DistanceScalerView;
 
 import java.awt.geom.Point2D;
 
-
+/**
+ * The CanvasExtrasController handles all elements that should be
+ * displayed on top of the canvas, but not directly interacting
+ * with the canvas.
+ */
 public class CanvasExtrasController extends Controller {
 
     private static CanvasExtrasController instance;
@@ -16,6 +20,9 @@ public class CanvasExtrasController extends Controller {
         super();
     }
 
+    /**
+     * Lazy singleton getinstance method
+     */
     public static CanvasExtrasController getInstance() {
         if (instance == null) {
             instance = new CanvasExtrasController();
@@ -23,15 +30,27 @@ public class CanvasExtrasController extends Controller {
         return instance;
     }
 
+    /**
+     * Sets up extra elements on top of the canvas
+     */
     public void setupExtras() {
         distanceScaler = new DistanceScalerView();
         distanceScaler.setVisible(true);
     }
 
+    /**
+     * Returns the DistanceScaler view component
+     */
     public DistanceScalerView getDistanceScaller() {
         return distanceScaler;
     }
 
+    /**
+     * Updates the displayed distance on the distance scaler
+     * (Found by taking the distance between the upper-leftmost corner and
+     * a point approx. 100 pixels to the right of it and converting those points
+     * into model coordinates such that a distance could be calculated)
+     */
     public void updateDistance() {
         Point2D start = new Point2D.Float(0,0);
         Point2D to = new Point2D.Float(distanceScaler.getWidth(),0);
@@ -42,6 +61,9 @@ public class CanvasExtrasController extends Controller {
         distanceScaler.repaint();
     }
 
+    /**
+     * Updates the theme of the visual components
+     */
     public void themeHasChanged() {
         distanceScaler.themeChanged();
     }
