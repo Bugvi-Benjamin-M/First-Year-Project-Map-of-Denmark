ea996254a49083103f195cbb46b7d7d4e2422d7d
Búgvi Magnussen <bugvimagnussen@gmail.com>
2017-05-15 15:26:31 +0200

    Optimising GUI

--- src/Controller/MainWindowController.java
+++ src/Controller/MainWindowController.java
@@ -70,7 +70,9 @@ public final class MainWindowController extends WindowController {
                      .icon()
                      .hide();
         //Todo, optimise window height minimum
-        window.setMinimumWindowSize(new Dimension(ToolbarController.getSmallLargeEventWidth()-FROM_RESIZE_EVENT_TO_MINIMUMWIDTH,(int) ((Toolkit.getDefaultToolkit().getScreenSize().getHeight()) /1.3)));
+        if(!OSDetector.isWindows()) window.setMinimumWindowSize(new Dimension(ToolbarController.getSmallLargeEventWidth()-FROM_RESIZE_EVENT_TO_MINIMUMWIDTH, 650));
+        else window.setMinimumWindowSize(new Dimension(ToolbarController.getSmallLargeEventWidth()-FROM_RESIZE_EVENT_TO_MINIMUMWIDTH, 660));
+        System.out.println(Toolkit.getDefaultToolkit().getScreenSize().getHeight());
         setupToolbar();
         setupCanvas();
         setupCanvasExtras();
