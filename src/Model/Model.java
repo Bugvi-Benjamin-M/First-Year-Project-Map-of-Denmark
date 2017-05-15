db931081c253a57a9a0faa37eb0c2e4b023bc2a7
Niclas Hedam <niclas@frax.dk>
2017-05-15 19:25:09 +0200

    some model docs

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -16,7 +16,10 @@ import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.util.*;
 
-
+/*
+ * The model describes the state of the application, inlcuding point of interests,
+ * ways and the graph. It is a singleton.
+ */
 public final class Model extends Observable {
 
     private EnumMap<ElementType, KDTree> elements;
@@ -37,6 +40,9 @@ public final class Model extends Observable {
     private ArrayList<POI> pointsOfInterest;
     private float longitudefactor = Float.POSITIVE_INFINITY;
 
+    /*
+     * Construct a new model. Initializes the relevant fields
+     */
     private Model()
     {
         bounds = new EnumMap<>(BoundType.class);
@@ -57,7 +63,9 @@ public final class Model extends Observable {
         coastlineFactory = Helpers.FileHandler.loadCoastlines();
         pointsOfInterest = new ArrayList<>();
     }
-
+    /*
+     * Gets the singleton instance of the Model.
+     */
     public static Model getInstance()
     {
         if (instance == null) {
@@ -66,16 +74,26 @@ public final class Model extends Observable {
         return instance;
     }
 
+    /*
+     * Gets the graph factory from the model. The factory is used to generate
+     * graphs to run Dijkstra on.
+     */
     public RoadGraphFactory getGraphFactory(){
         if (graphFactory == null) throw new NullPointerException("Factory has not been created");
         return this.graphFactory;
     }
 
+    /*
+     * Gets the graph of the model.
+     */
     public RoadGraph getGraph() {
         if (graphFactory == null) throw new NullPointerException("The graph has not been initialized");
         else return graphFactory.getGraph();
     }
 
+    /*
+     * Sets the current graph
+     */
     public void setGraph(RoadGraph graph) {
         if (graph == null) throw new IllegalArgumentException("Graph object must not be null");
         if (graphFactory == null) {
@@ -85,48 +103,84 @@ public final class Model extends Observable {
         }
     }
 
+    /*
+     * Create a graph from a set of roads. This will also construct it.
+     */
     public void createGraph(HashSet<SuperElement> roads) {
         if (roads == null) throw new IllegalArgumentException("Collection is not initialized, must not be null");
         graphFactory = new RoadGraphFactory(roads);
     }
 
+    /*
+     * Converts the city index to a city name
+     */
     public String getIndexToCity(int index){
         return indexToCity.get(index);
     }
 
+    /*
+     * Converts the city name to a city index.
+     */
     public int getCityToIndex(String cityName){
         return cityToIndex.get(cityName);
     }
 
+    /*
+     *  Checks whether the city entry exists
+     */
     public boolean cityEntryExists(String cityName){
         return cityToIndex.containsKey(cityName);
     }
 
+    /*
+     * Inserts a city into the city index.
+     */
     public void putCityToIndex(String cityName, int i){
             cityToIndex.put(cityName, i);
             indexToCity.put(i, cityName);
     }
 
+    /*
+     * Returns the city index map.
+     */
     public HashMap<String, Integer> getCityToIndexMap(){
         return cityToIndex;
     }
 
+    /*
+     * Sets the city index map
+     */
     public void setCityToIndexMap(HashMap<String, Integer> map){
         this.cityToIndex = map;
     }
 
+    /*
+     * gets index to city map
+     */
     public HashMap<Integer, String> getIndexToCityMap(){
         return indexToCity;
     }
 
+    /*
+     * Sets the city to index maps
+     */
     public void setIndexToCityMap(HashMap<Integer, String> map){
         this.indexToCity = map;
     }
 
+    /*
+     * Gets all the elements from the model.
+     */
     public EnumMap<ElementType, KDTree> getElements() { return elements; }
 
+    /*
+     * Gets all the elements of the given type
+     */
     public KDTree getElements(ElementType type) {return elements.get(type);}
 
+    /*
+     * Sets the elements of the given type.
+     */
     public void setElements(EnumMap<ElementType, KDTree> elements)
     {
         this.elements = elements;
