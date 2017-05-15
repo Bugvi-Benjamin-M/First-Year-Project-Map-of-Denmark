6daf20593ae6df973cc67095552741fedf421779
Niclas Hedam <niclas@frax.dk>
2017-05-15 18:55:17 +0200

    a few more docs

--- src/Model/Elements/Rail.java
+++ src/Model/Elements/Rail.java
@@ -5,10 +5,17 @@ import Helpers.Shapes.PolygonApprox;
 public class Rail extends Element {
     private boolean isInTunnel;
 
+    /**
+     * Constructs a new rail
+     */
     public Rail(PolygonApprox polygon, boolean isInTunnel){
         super(polygon);
         this.isInTunnel = isInTunnel;
     }
+
+    /**
+     * Gets the shape of the rail line
+     */
     public PolygonApprox getShape() { return (PolygonApprox)super.getShape(); }
 
     /**
