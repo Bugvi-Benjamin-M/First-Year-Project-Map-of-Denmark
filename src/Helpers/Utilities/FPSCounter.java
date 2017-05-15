15ad328090ffebed6c54ac27ce89458eafbd5951
Andreas Blanke <andreas.blanke.dk@gmail.com>
2017-05-15 17:56:55 +0200

    javadoc for helper utilities

--- src/Helpers/Utilities/FPSCounter.java
+++ src/Helpers/Utilities/FPSCounter.java
@@ -1,6 +1,12 @@
 package Helpers.Utilities;
 
-
+/**
+ * The FPSCounter is a Thread that runs alongside the other
+ * threads in the program (such as GUI event thread and main
+ * thread) and calculates how often it is possible to update.
+ * Used by MapCanvas to calculate how many often the canvas is
+ * updated per second.
+ */
 public class FPSCounter extends Thread {
     private long lastInterupt;
     private double fps;
@@ -10,6 +16,9 @@ public class FPSCounter extends Thread {
 
     public FPSCounter() { stopThread = false; }
 
+    /**
+     * The runtime loop of the thread. Continues until stopped
+     */
     @Override
     public void run()
     {
@@ -20,16 +29,25 @@ public class FPSCounter extends Thread {
             } catch (InterruptedException e) {
                 // System.out.println("Thread has been interrupted.");
             }
-            // one second(nano) divided by amount of time it takes for one frame to
-            // finish
+            // one second(nano) divided by amount of time it takes for
+            // one frame to finish
             fps = NANO_TO_SECOND / (System.nanoTime() - lastInterupt);
         }
     }
 
+    /**
+     * Stops the thread from running for a while
+     */
     public void stopThread() { stopThread = true; }
 
+    /**
+     * Retrieves the last FPS value
+     */
     public double getFPS() { return fps; }
 
+    /**
+     * Retrieves a string representing the FPS value
+     */
     @Override
     public String toString()
     {
